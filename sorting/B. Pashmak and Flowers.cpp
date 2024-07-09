10cdff85194bb6022bc72cb75fbf410c05d95f6f
faroukq1 <faroukq1@gmail.com>
2024-07-09 12:26:10 +0100

    B. Pashmak and Flowers

--- sorting/B. Pashmak and Flowers.cpp
+++ sorting/B. Pashmak and Flowers.cpp
@@ -5,23 +5,23 @@ int main() {
   int N;
   cin >> N;
   vector<int> arr(N);
-  for (int i = 0; i < N; i++)
-    cin >> arr[i];
+
+  for (auto &a : arr)
+    cin >> a;
 
   sort(arr.begin(), arr.end());
 
   int low = arr[0], high = arr[arr.size() - 1];
-  int numOfLow = count(arr.begin(), arr.end(), low);
-  int numOfHigh = count(arr.begin(), arr.end(), high);
+  int minCount = count(arr.begin(), arr.end(), low);
+  int maxCount = count(arr.begin(), arr.end(), high);
 
   long long combine;
+
   if (low == high) {
     combine = (long long)N * (N - 1) / 2;
   } else {
-    combine = (long long)numOfLow * numOfHigh;
+    combine = (long long)minCount * maxCount;
   }
 
   cout << high - low << ' ' << combine << endl;
-
-  return 0;
 }
