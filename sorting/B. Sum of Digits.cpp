9846e4bf7949a0ab0e66f99a8d76bca731b79ced
faroukq1 <faroukq1@gmail.com>
2024-07-09 12:29:21 +0100

    B. Sum of Digits

--- sorting/B. Sum of Digits.cpp
+++ sorting/B. Sum of Digits.cpp
@@ -1,26 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+int main() {
+  string N;
+  cin >> N;
+  if (N.length() < 2) {
+    cout << 0;
+    return 0;
+  }
 
-int main () {
-    string N;
-    cin >> N;
-    if (N.length() < 2) {
-        cout << 0;
-        return 0;
-    }
+  int counter = 0, x = 11;
+  while (x >= 10) {
+    x = 0;
+    for (int i = 0; i < N.length(); i++)
+      x = x + (N[i] - '0');
 
+    N = to_string(x);
 
-    int cpt = 0 , x = 11;
-    while (x >= 10) {
-        x = 0;
-        for (int i = 0 ; i < N.length() ;i++)
-            x = x + (N[i] - '0');
+    counter++;
+  }
 
-        N = to_string(x);
-
-        cpt++;
-    }
-
-    cout << cpt;
+  cout << counter;
 }
