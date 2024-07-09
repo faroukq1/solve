cbe1d43c45ceccc5e7cdc5696f5f7be00d2ccc98
faroukq1 <faroukq1@gmail.com>
2024-07-09 12:31:31 +0100

    balancedround

--- sorting/VaniaAndLanterns.cpp
+++ sorting/VaniaAndLanterns.cpp
@@ -1,32 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int n , l;
-    cin >> n >> l;
+  int n, l;
+  cin >> n >> l;
 
-    vector<int> pos(n);
+  vector<int> pos(n);
 
-    for (auto &p : pos)
-        cin >> p;
+  for (auto &p : pos)
+    cin >> p;
 
-    sort(pos.begin(),pos.end());
+  sort(pos.begin(), pos.end());
 
-    
+  double maxGap = 0.0;
 
-    double maxGap = 0.0;
+  for (int i = 1; i < n; i++)
+    maxGap = max(maxGap, (double)(pos[i] - pos[i - 1]));
 
-    for (int i = 1 ; i < n ; i++)
-        maxGap = max(maxGap , (double)(pos[i] - pos[i - 1]));
+  double maxRad = max((double)pos[0], (double)(l - pos[n - 1]));
+  maxRad = max(maxRad, maxGap / 2.0);
 
-
-
-    double maxRad = max((double)pos[0] , (double)(l - pos[n - 1]));
-    maxRad = max(maxRad , maxGap / 2.0);
-
-    cout << fixed << setprecision(10) << maxRad;
-
-
+  cout << fixed << setprecision(10) << maxRad << endl;
 }
-
-
