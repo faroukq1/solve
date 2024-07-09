7b8ae9aae5bf779bb857b308f27c9f302d1e845c
faroukq1 <faroukq1@gmail.com>
2024-07-09 12:32:37 +0100

    Sale

--- sorting/sale.cpp
+++ sorting/sale.cpp
@@ -1,32 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+int main() {
+  int m, n;
+  cin >> m >> n;
+  vector<int> tvs(m);
 
-int main () {
-    int m , n;
-    cin >> m >> n;
-    vector<int> tvs(m);
+  for (auto &m : tvs)
+    cin >> m;
 
-    for (auto &m : tvs)
-        cin >> m;
+  sort(tvs.begin(), tvs.end());
 
-    sort(tvs.begin(),tvs.end());
+  int ans = 0;
 
-    int ans = 0;
+  for (int i = 0; i < tvs.size(); i++)
+    if (tvs[i] < 0) {
+      ans = ans + tvs[i];
+      n--;
+      if (n == 0)
+        break;
+    } else
+      break;
 
-    for (int i = 0 ; i < tvs.size() ; i++)
-        if(tvs[i] < 0) {
-            ans = ans + tvs[i];
-            n--;
-            if (n == 0)
-                break;
-        } else
-            break;
-
-    cout << -ans;
-
-
-    return 0;
+  cout << -ans;
 }
-
-
