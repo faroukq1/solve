fa1c28fa6d189545191c0ba7c648322ba3bbeddc
faroukq1 <faroukq1@gmail.com>
2024-07-09 12:29:58 +0100

    Dragons

--- sorting/Dragons.cpp
+++ sorting/Dragons.cpp
@@ -1,28 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+int main() {
+  int s, n;
+  cin >> s >> n;
+  vector<pair<int, int>> dragons(n);
+  for (int i = 0; i < n; i++) {
+    int temp1, temp2;
+    cin >> temp1 >> temp2;
+    dragons[i] = {temp1, temp2};
+  }
 
-int main () {
-    int s,n;
-    cin >> s >> n;
-    vector<pair <int , int > > dragons(n);
-    for (int i = 0 ; i < n; i++)  {
-        int temp1 ,temp2;
-        cin >> temp1 >> temp2;
-        dragons[i] = {temp1 , temp2};
+  sort(dragons.begin(), dragons.end());
+
+  for (int i = 0; i < dragons.size(); i++) {
+    if (s <= dragons[i].first) {
+      cout << "NO";
+      return 0;
+    } else {
+      s = s + dragons[i].second;
     }
+  }
 
-    sort(dragons.begin(),dragons.end());
-
-
-    for (int i = 0 ; i < dragons.size(); i++) {
-        if (s <= dragons[i].first) {
-            cout << "NO";
-            return 0;
-        } else {
-            s =  s + dragons[i].second;
-        } 
-    }
-
-    cout << "YES";
+  cout << "YES" << endl;
 }
