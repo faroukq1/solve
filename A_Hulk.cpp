7f7843f82b921a4cbcae9b06a5aa318e5ab02ed7
faroukq1 <faroukq1@gmail.com>
2024-07-29 22:47:38 +0100

    A_Hulk

--- A_Hulk.cpp
+++ A_Hulk.cpp
@@ -13,29 +13,24 @@ const int MXN = 1e5 + 5, INF = 1e9 + 5;
 signed main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
-  int N;
-  cin >> N;
-  if (N == 1) {
-    cout << "I hate it ";
-    return 0;
-  } else if (N == 2) {
-    cout << "I hate that I love it";
-    return 0;
-  } else if (N == 3) {
-    cout << "I hate that I love that I hate it";
-    return 0;
-  } else {
-    for (int i = 1; i <= N; i++) {
-      if (i == N && N % 2 == 0) {
-        cout << "I love that";
-      } else if (i == N && N % 2 != 0) {
-        cout << "I hate that";
-      }
+  int n;
+  cin >> n;
+  if (n == 1)
+    cout << "I hate it";
+  else if (n > 1)
+    cout << "I hate that";
+  for (int i = 2; i < n; i++) {
 
-      if (i % 2 == 0)
-        cout << "I love it" << ' ';
-      else
-        cout << "I hate it" << ' ';
-    }
+    if (i % 2 == 1 && i != n)
+      cout << " I hate that";
+
+    else if (i % 2 == 0 && i != n)
+      cout << " I love that";
   }
+  if (n % 2 == 0 && n > 1)
+    cout << " I love it";
+  else if (n > 1)
+    cout << " I hate it";
+  cout << endl;
+  return 0;
 }
