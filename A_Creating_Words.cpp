00822f31999f866691c9d01c3a28ad43c7f1441b
faroukq1 <faroukq1@gmail.com>
2024-07-30 22:47:26 +0100

    A_Createing_Words

---
+++ A_Creating_Words.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+const int MXN = 1e5 + 5, INF = 1e9 + 5;
+
+void solve() {
+  string a, b;
+  cin >> a >> b;
+  char temp = b[0];
+  b[0] = a[0];
+  a[0] = (char)temp;
+
+  cout << a << ' ' << b << "\n";
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int tt;
+  cin >> tt;
+  while (tt--)
+    solve();
+
+  return 0;
+}
