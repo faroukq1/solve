f02a6ad5089ac00cda67e2b00fb7eeba485c8294
faroukq1 <faroukq1@gmail.com>
2024-07-30 21:55:43 +0100

    A_Special_Permutation

---
+++ A_Special_Permutation.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+const int MXN = 1e5 + 5, INF = 1e9 + 5;
+
+void solve() {
+  int N;
+  cin >> N;
+  vector<int> A(N);
+
+  for (int i = 0; i < N; i++)
+    A[i] = (i + 1) % N;
+
+  for (auto a : A)
+    cout << a + 1 << ' ';
+
+  cout << "\n";
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int tt;
+  cin >> tt;
+  while (tt--)
+    solve();
+  return 0;
+}
