9c9df76fe2d08d85b302afc12bb1f63d6005315d
faroukq1 <faroukq1@gmail.com>
2024-08-03 09:58:44 +0100

    A_Olesya_and_Rodion

--- A_Olesya_and_Rodion.cpp
+++ A_Olesya_and_Rodion.cpp
@@ -17,20 +17,18 @@ int main() {
 
   if (t == 10) {
     if (n == 1) {
-      cout << -1 << endl;
+      cout << -1;
+      return 0;
     } else {
       cout << 1;
-      for (int i = 1; i < n; i++) {
+      for (int i = 1; i < n; i++)
         cout << 0;
-      }
-      cout << endl;
+
+      return 0;
     }
   } else {
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
       cout << t % 10;
-    }
-    cout << endl;
+    return 0;
   }
-
-  return 0;
 }
