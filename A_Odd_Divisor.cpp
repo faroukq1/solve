66023db94142d9f1714f6b457b428823b9d6ff39
faroukq1 <faroukq1@gmail.com>
2024-08-09 10:07:10 +0100

    A_Odd_Divisor

---
+++ A_Odd_Divisor.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+const int MXN = 1e5 + 5, INF = 1e9 + 5;
+using ll = long long;
+
+bool isPowerOfTwo(ll n) { return (n & (n - 1)) == 0; }
+void solve() {
+  ll n;
+  cin >> n;
+  if (isPowerOfTwo(n))
+    cout << "NO" << endl;
+  else
+    cout << "YES" << endl;
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  ll tt;
+  cin >> tt;
+  while (tt--)
+    solve();
+  return 0;
+}
