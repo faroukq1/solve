167f96b924daef9e29fd66ba0b7c1c41a6122601
faroukq1 <faroukq1@gmail.com>
2024-08-12 09:47:26 +0100

    A_Almost_Prime

--- B_Chemistry.cpp
+++ B_Chemistry.cpp
@@ -8,38 +8,27 @@ template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
 }
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 
-bool isPalindrom (string &s) {
-    int i = 0 , j = s.length() - 1;
-    while (i <= j)
-        if (s[i] != s[j])
-            return false;
-    return true;
+const int MXN = 1e5 + 5, INF = 1e9 + 5;
+
+void solve() {
+  int n, k;
+  string a;
+  cin >> n >> k >> a;
+  if (n == 1) {
+    cout << "YES" << endl;
+    return;
+  } else if (k == 0 && n == 2) {
+    cout << "NO";
+    return;
+  }
 }
 
-
-void solve () {
-    int n , k;
-    cin >> n >> k;
-    vector<char> arr(n);
-    if (n == 1) {
-        cout << "YES" << endl;
-        return;
-    }
-
-    if (n == 2 && k == 0) {
-        cout << "NO" << endl;
-        return
-    }
-
-    for (auto &a : arr)
-        cin >> a;
-}
-
-
-int main () {
-    int tt;
-    cin >> tt;
-    while (tt--)
-        solve();
-    return 0;
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int tt;
+  cin >> tt;
+  while (tt--)
+    solve();
+  return 0;
 }
