c7fd162415a5a67a477a729d92a8a31825386ea4
faroukq1 <faroukq1@gmail.com>
2024-08-17 11:59:54 +0100

    A_George_and_Accomodation

---
+++ A_George_and_Accommodation.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+const int MXN = 1e5 + 5, INF = 1e9 + 5;
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, a, b, cpt = 0;
+  cin >> n;
+  while (n--) {
+    cin >> a >> b;
+    if (b - a >= 2)
+      cpt++;
+  }
+
+  cout << cpt << endl;
+  return 0;
+}
