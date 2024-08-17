55bf146647c25f6cfb2aa48fb5640f06363d24be
faroukq1 <faroukq1@gmail.com>
2024-08-17 11:28:35 +0100

    B_Comparison_String

---
+++ B_Comparison_String.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+const int MXN = 1e5 + 5, INF = 1e9 + 5;
+
+void solve() {
+  int N, cpt = 1, mx = 0;
+  string s;
+  cin >> N >> s;
+  for (int i = 1; i < N; i++)
+    if (s[i - 1] == s[i])
+      cpt++;
+    else {
+      mx = max(mx, cpt);
+      cpt = 1;
+    }
+
+  mx = max(mx, cpt);
+  cout << mx + 1 << endl;
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int tt;
+  cin >> tt;
+  while (tt--)
+    solve();
+  return 0;
+}
