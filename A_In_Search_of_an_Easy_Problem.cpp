0fcbc30c40b9050d989cef388181549237bdcd96
faroukq1 <faroukq1@gmail.com>
2024-08-17 11:50:05 +0100

    In Search of an Easy Problem

---
+++ A_In_Search_of_an_Easy_Problem.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+const int MXN = 1e5 + 5, INF = 1e9 + 5;
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int N, a;
+  cin >> N;
+
+  while (N--) {
+    cin >> a;
+    if (a == 1) {
+      cout << "HARD";
+      return 0;
+    }
+  }
+
+  cout << "EASY";
+
+  return 0;
+}
