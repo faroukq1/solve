db841ba97349e4ce9daf664705e5a68c5b7e9fff
faroukq1 <faroukq1@gmail.com>
2024-08-19 11:34:46 +0100

    1703A - YES or YES?

---
+++ A_YES_or_YES.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+const int MXN = 1e5 + 5, INF = 1e9 + 5;
+
+void solve() {
+  string s;
+  cin >> s;
+  auto valid = [&](string &s) -> bool {
+    return tolower(s[0]) == 'y' && tolower(s[1]) == 'e' && tolower(s[2]) == 's';
+  };
+
+  cout << (valid(s) ? "YES" : "NO") << endl;
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int tt;
+  cin >> tt;
+  while (tt--)
+    solve();
+  return 0;
+}
