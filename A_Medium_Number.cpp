117361b10b1f986dbdb5ca87962127eae9581829
faroukq1 <faroukq1@gmail.com>
2024-08-19 16:10:01 +0100

    1760A - Medium Number

---
+++ A_Medium_Number.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+const int MXN = 1e5 + 5, INF = 1e9 + 5;
+
+void solve() {
+  vector<int> arr(3);
+  for (auto &a : arr)
+    cin >> a;
+
+  sort(arr.begin(), arr.end());
+
+  cout << arr[1] << endl;
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int tt;
+  cin >> tt;
+  while (tt--)
+    solve();
+  return 0;
+}
