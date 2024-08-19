3efb6977fa8d2c91a2db957850c285e497ecce78
faroukq1 <faroukq1@gmail.com>
2024-08-19 16:04:05 +0100

    1850A - To My Critics

---
+++ A_To_My_Critics.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+const int MXN = 1e5 + 5, INF = 1e9 + 5;
+
+void solve() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  bool weCan = a + b >= 10 || a + c >= 10 || b + c >= 10;
+  cout << (weCan ? "YES" : "NO") << endl;
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int tt;
+  cin >> tt;
+  while (tt--)
+    solve();
+  return 0;
+}
