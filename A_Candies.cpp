cfc49a9c53c61217b738329beb9f1741d2b88fde
faroukq1 <faroukq1@gmail.com>
2024-08-23 07:55:48 +0100

    1343A - Candies

--- A_Candies.cpp
+++ A_Candies.cpp
@@ -13,28 +13,19 @@ const int MXN = 1e5 + 5, INF = 1e9 + 5;
 void solve() {
   long long N;
   cin >> N;
-  // 3 : 1 * 1 + 2 * 1 = 3 (k == 2)
-  // 6 : 1 * 2 + 2 * 2 = 6 (k == 6)
-  long long x = 1;
-  long long cpt = 0;
-  for (long long k = 3; k < 100; k++) {
-    while (k >= 1) {
-      cpt += (k * x);
-      cout << cpt << ' ';
-      if (cpt == N)
-        break;
+  long long k = 1, sum = 1;
+  while (true) {
+    sum += pow(2, k++);
+    if (N % sum == 0) {
+      cout << N / sum << endl;
+      break;
     }
   }
-  cout << endl;
 }
 
 signed main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
-  // there is n candy wrappers
-  // he bought x candies during the first day
-  // 2x the second ... pow(2 , k - 1) candies during the k-th day
-  // x > 0 && k > 1
   int tt;
   cin >> tt;
   while (tt--)
