a3522cbc67b0157025016cb017e864de060f1b50
faroukq1 <faroukq1@gmail.com>
2024-09-03 23:00:19 +0100

    The Legend of Freya the Frog

--- The Legend of Freya the Frog.cpp
+++ The Legend of Freya the Frog.cpp
@@ -9,18 +9,17 @@ template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 
 
+int ceil_div (int a , int b) {
+    return a / b + ((a ^ b) > 0 && a % b != 0);
+}
+
 void solve () {
     int a , b , c;
     cin >> a >> b >> c;
 
-    int x_moves = a / c , y_moves = b / c;
-    if (a % c != 0)
-        x_moves++;
-    if (b % c != 0)
-        y_moves++;
-    
-    
-    cout << max(x_moves , y_moves) * 2 << endl;
+    int x_dir = ceil_div(a , c), y_dir = ceil_div(b ,c);
+
+    cout << max(x_dir * 2 - 1 , y_dir * 2) << endl;
 }
 
 
