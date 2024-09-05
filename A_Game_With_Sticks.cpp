a069043f4358b0f85eb0f8733d074e0d1acaed6e
faroukq1 <faroukq1@gmail.com>
2024-09-05 09:32:16 +0100

    451A - Game With Sticks

--- A_Game_With_Sticks.cpp
+++ A_Game_With_Sticks.cpp
@@ -8,18 +8,10 @@ template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
 }
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 
-const int MXN = 1e5 + 5, INF = 1e9 + 5;
 
-signed main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  int n, m;
-  cin >> n >> m;
-  const int total = n * m;
-  const int sticks = n + m;
-  if (total % 2 == 0 && sticks % 2 == 1)
-    cout << "Malvika";
-  else
-    cout << "Akshat";
-  return 0;
+int main () {
+  int a , b;
+  cin >> a >> b;
+
+  cout << (min(a , b) % 2 == 0 ? "Malvika" : "Akshat");
 }
