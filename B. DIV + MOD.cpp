252b97429f4884c0282fd7d6381e65b6c6b5bc0c
faroukq1 <faroukq1@gmail.com>
2024-09-05 09:51:29 +0100

    my own solution 1650B - DIV + MOD

--- B. DIV + MOD.cpp
+++ B. DIV + MOD.cpp
@@ -9,15 +9,15 @@ template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 
 void solve() {
-    int64_t l, r, a;
-    cin >> l >> r >> a;
-    
-    int64_t max_value = (r / a) + (r % a);
-    
-    int64_t x = r - (r % a) - 1;
-    if (x >= l) {
-        max_value = max(max_value, (x / a) + (x % a));
-    }
+    int64_t a , b , c;
+    cin >> a >> b >> c;
+
+    int64_t max_value = (b / c) + (b % c);
+
+    int64_t next_mult = b - (b % c) - 1;
+
+    if (next_mult >= a)
+        max_value = max(max_value ,(next_mult / c) + (next_mult % c));
 
     cout << max_value << endl;
 }
