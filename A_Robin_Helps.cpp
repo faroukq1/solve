89ad667bdb5235fb48c181af0700339fc4493f02
faroukq1 <faroukq1@gmail.com>
2024-09-22 06:26:55 +0100

    A_Robin_helps

---
+++ A_Robin_Helps.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+void solve () {
+    int N , K;
+    cin >> N >> K;
+    int gold = 0 , ans = 0;
+
+    for (int i = 0 , a; i < N ; i++) {
+        cin >> a;
+        if (a >= K)
+            gold += a;
+
+        if (a == 0 && gold > 0) {
+            gold--;
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
+}
+int main () {
+    int tt;
+    cin >> tt;
+    while (tt--)
+        solve();
+    return 0;
+}
