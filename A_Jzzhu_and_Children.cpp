bc3d53f2838cbcba842ee27d498cf0af49689b6e
faroukq1 <faroukq1@gmail.com>
2024-09-25 14:41:23 +0100

    A_Jzzhu_and_Children

--- A_Jzzhu_and_Children.cpp
+++ A_Jzzhu_and_Children.cpp
@@ -1,38 +1,45 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n, m;
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+
+void solve () {
+    int n , m;
     cin >> n >> m;
-    
-    queue<pair<int, int>> q;  
-    vector<int> a(n);
-    
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-        q.push({i + 1, a[i]});  
+    queue< pair<int , int >> q;
+    vector<int> arr(n);
+
+    for (int i = 0 ; i < n ; i++) {
+        cin >> arr[i];
+        q.push({i + 1, arr[i]});
     }
-    
     int last_child = -1;
-    
+
     while (!q.empty()) {
-        auto current = q.front();
+        auto curr = q.front();
         q.pop();
+
+        int index = curr.first;
+        int remaining_candies = curr.second - m;
+        if (remaining_candies > 0) 
+            q.push({index , remaining_candies});
         
-        int index = current.first;
-        int remaining_candies = current.second;
-        
-        remaining_candies -= m;
-        
-        if (remaining_candies > 0) {
-            q.push({index, remaining_candies});
-        }
-        
+
         last_child = index;
     }
-    
+
     cout << last_child << endl;
-    
+}  
+int main () {
+    int tt = 1;
+    // cin >> tt;
+    while (tt--)
+        solve();
     return 0;
 }
