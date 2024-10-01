37b7a9b9c0fe26a625a031364deeab54b28a4f15
faroukq1 <faroukq1@gmail.com>
2024-10-01 11:06:34 +0100

    not solved yet

---
+++ sces fi/Dice Combinations.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+long long MOD = 10e9 + 7;
+
+int main () {
+    int N;
+    cin >> N;
+    
+    return 0;
+}
