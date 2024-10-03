6d2e7398bc8e8a13af46093e7cda84f0efbb2d54
faroukq1 <faroukq1@gmail.com>
2024-10-03 10:07:47 +0100

    finish vectors

--- algorithms & patterns/c++ STL/c++stl-vector.cpp
+++ algorithms & patterns/c++ STL/c++stl-vector.cpp
@@ -108,7 +108,147 @@ int main () {
     vec.erase(vec.begin() + 1); // removing the second elment O(N)
     vec.insert(vec.begin() + 1 , 10); // inserting at pos 1 the value = 10 O(N)
     output_vector(vec);
+
+
+    // how to add element from the back
+    vec.push_back(10);
+    output_vector(vec);
+
+
+    // how to reverse array
+    reverse(vec.begin(),vec.end());
+
+    // how to remove element from the last
+    vec.pop_back();
+
+    // how to clear vec
+    vector<int> clear = vec;
+    cout << "vector before clearing : " << endl;
+    output_vector(clear);
+    clear.clear();
+    cout << "clearing the vector : " << endl;
+    output_vector(clear);
+
+
+    /*
+    MANIPULATION OF VECTORS
+    */
+
+    // ditance on vector
+    vector<int> manipulate;
+
+    for (int i = 0 ; i < 10 ; i++)
+        manipulate.push_back(i + 1);
+
+    // the distance between pos 2 and 4
+    // idk why this didn't work cout << "distnace : " << distane(manipulate.begin() + 2 , manipulate.begin() + 4);
+
+    // how to replace item on vector
+
+    replace(manipulate.begin(),manipulate.end() , 3 , 5);
+
+    // here replacing the 3 by 5
+
+    output_vector(manipulate);
+
+    // replace by predicate
+    auto odd = [&] (int a) -> bool {
+        return a & 1;
+    };
+    replace_if(manipulate.begin(),manipulate.end() , odd , 15);
+    // every odd element replace it by 15
+
+    /*
+    THIS MANIPULATION TECHNIQUE ONLY WORK
+    WITH SORTED ARRAY
+    SO WE HAVE TO SORT THE VECTOR BEFORE
+    USING IT
+    */ 
+
+
+    // sorting the array
+    sort(manipulate.begin(),manipulate.end());
+
+    // the binary search
+
+    int ans = binary_search(manipulate.begin(),manipulate.end() , 7);
+
+    cout << (ans != -1 ? "there is number = 7" : "we didn't find anythig") << endl;
+
+    // lower bound and upper bound
+
+    /*
+    lower bound allows you to get the first
+    value that equal or greater than the 
+    given value
+
+    upper bound is only give the greater element
+    than the given value
+    */ 
+
+    auto upper = upper_bound(manipulate.begin(),manipulate.end() , 10);
+
+    cout << *upper << ' ' << endl;
+
+
+    // unique : get the deblicate element and push it into the back
+
+    for (int i = 0 ; i < 3 ; i++)
+        manipulate.push_back(i + 1);
+
+    // how to remove deblicate
+    // in sorted array the unique give the index of the first
+    // deblicated element
+    // erase it with .end() eterator
+
+    manipulate.erase(unique(manipulate.begin(),manipulate.end()) , manipulate.end());
+
+    output_vector(manipulate);
+
+    // how to get the max element
+    int mx = *max_element(manipulate.begin(),manipulate.end());
+
+    // how to get them min element
+    int mn = *min_element(manipulate.begin(),manipulate.end());
+
+    cout << "min value : " << mn << "max value : " << mx << endl;
+
+    // how to get the sum of the vector
+    // acumulate(eterators , initial value);
+    int sum = accumulate(manipulate.begin(),manipulate.end() , 0);
+    // get the mult of the vector
+
+    int mult = accumulate(manipulate.begin(),manipulate.end() , 1 , multiplies<int>());
+
+    cout << "sum of vector : " << sum << "mult of vector : " << mult << endl;
+    // how to get the inner product
+    // v[0] * v[1] + v1[0] * v1[1] ...
+    int inner_product_of_two_vectors = inner_product(manipulate.begin(),manipulate.end() , vec.begin() , 0);
+    cout << "the inner product of manipulate and vec : " << endl;
+    cout << inner_product_of_two_vectors << ' ' << endl;
+
+    // remove such elements
+    manipulate.erase(remove(manipulate.begin(),manipulate.end() , 3) , manipulate.end());
+
+    // items with value = 6 has been removed
+
+    // how to rotate vector
+
+    rotate(manipulate.begin(),manipulate.end() , manipulate.end());
+
+    output_vector(manipulate);
+
+
+    // // get permutation of vector
+
+    // sort(manipulate.begin(),manipulate.end());
+
+    // do {
+    //     output_vector(manipulate);
+    // } while (next_permutation(manipulate.begin(),manipulate.end()));
+
     
+
     #ifndef ONLINE_JUDGE
     function<bool(string,string)> compareFiles = [](string p1, string p2)->bool {
         std::ifstream file1(p1);
