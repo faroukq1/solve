b46d6d92252ec1619c0cd6d87139975c6a3705a7
faroukq1 <faroukq1@gmail.com>
2024-10-04 11:57:42 +0100

    directed & undrected graph

--- algorithms & patterns/graph theory/graph.cpp
+++ algorithms & patterns/graph theory/graph.cpp
@@ -18,6 +18,14 @@ void add_edges_on_adj_matrix (vector<vector<int>>& adj_matrix, int u , int v) {
     adj_matrix[u][v] = 1;
     adj_matrix[v][u] = 1;
 }
+
+void add_direct_edge_adj_list (vector<vector<int>>& adj_list , int u , int v) {
+        adj_list[u].push_back(v);
+}
+
+void add_direct_edge_adj_matrix (vector<vector<int>>& adj_matrix , int u , int v) {
+    adj_matrix[u][v] = 1;
+}
 int main () {
     // how to declare a undirective-graph
     // vector<vector< type >> adj_list( size );
@@ -43,4 +51,17 @@ int main () {
     // adding edges between 0 and 1
     add_edges_on_adj_matrix(adj_matrix , 1 , 2);
     add_edges_on_adj_matrix(adj_matrix , 2 , 1);
+
+
+    /*
+    the directed graph is similar to the undirected graph
+    but the different is the you adsd only v to adjlist[u]
+    , indicating a one-way edge from u to v
+     */
+
+    vector<vector<int>> adj_list_directed(N);
+
+    // the edge 1 can go the edge == 2 but the 2 can't
+    add_direct_edge_adj_list(adj_list_directed , 1 , 2);
+
 }
