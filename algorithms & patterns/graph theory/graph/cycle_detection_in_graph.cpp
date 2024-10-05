5e6ade34137f7920c28164488c8c783ff3f4cd23
faroukq1 <faroukq1@gmail.com>
2024-10-05 11:07:27 +0100

    cycle in undrected graph

--- algorithms & patterns/graph theory/graph/cycle_detection_in_graph.cpp
+++ algorithms & patterns/graph theory/graph/cycle_detection_in_graph.cpp
@@ -18,7 +18,7 @@ bool dfs_helper (int node , int parent , vector<vector<int>>& adj , vector<bool>
 
     for (auto neighbor : adj[node])
         if (!visited[neighbor])
-            if (dfs(neighbor , node , adj , visited))
+            if (dfs_helper(neighbor , node , adj , visited))
                 return true;
         else if (neighbor != parent)
             return true;
@@ -26,15 +26,32 @@ bool dfs_helper (int node , int parent , vector<vector<int>>& adj , vector<bool>
     return false;
 }
 
-bool has_cycle_undrected (int N , vector<vector<int>>& adj) {
+bool has_cycle_undirected (int N , vector<vector<int>>& adj) {
     vector<bool> visited(N , false);
     for (int i = 0 ; i < N ; i++)
         if (!visited[i])
-            if (dfs(i , -1 , adj , visited))
+            if (dfs_helper(i , -1 , adj , visited))
                 return true;
     return false;
 }
-int main () {
+
+
+int main() {
+    int n = 5; 
+    vector<vector<int>> adj(n); 
+
+    // Example graph with a cycle (0-1-2-3-0)
+    adj[0] = {1, 3};
+    adj[1] = {0, 2};
+    adj[2] = {1, 3};
+    adj[3] = {0, 2};
+    adj[4] = {}; // Node 4 is isolated (no edges)
+
+    if (has_cycle_undirected(n, adj)) {
+        cout << "cycle detected!" << endl;
+    } else {
+        cout << "no cycle detected." << endl;
+    }
 
     return 0;
 }
