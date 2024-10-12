cfce0bb43aff84250cafb2792249a6076232f712
faroukq1 <faroukq1@gmail.com>
2024-10-12 10:01:53 +0100

    merge recursion

--- algorithms & patterns/recursion/recursion.cpp
+++ algorithms & patterns/recursion/recursion.cpp
@@ -123,6 +123,76 @@ int recursive_binary_search (vector<int>& arr , int left , int right , int targe
     return recursive_binary_search(arr , mid + 1 , right , target);
 }
 
+long long fib_non_optimal (long long number) {
+  if (number == 0) return 1;
+  if (number == 1) return 1;
+
+  return fib_non_optimal(number - 1) + fib_non_optimal(number - 2); 
+}
+
+
+// we need map to memorize the call stacks
+map < long long , long long > memo;
+long long optimal_fib (long long number) {
+  // remove call stack redendency
+  if (number == 0) return 1;
+  if (number == 1) return 1;
+  if (memo.find(number) != memo.end()) return memo[number];
+
+  memo[number] = optimal_fib(number - 1) + optimal_fib(number - 2);
+
+  return memo[number];
+}
+
+
+// merge sort recursive
+
+void merge(vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1; 
+    int n2 = right - mid;    
+    vector<int> leftArr(n1), rightArr(n2);
+
+    for (int i = 0; i < n1; i++)
+        leftArr[i] = arr[left + i];
+    for (int i = 0; i < n2; i++)
+        rightArr[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left; 
+
+    while (i < n1 && j < n2) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k] = leftArr[i];
+            i++;
+        } else {
+            arr[k] = rightArr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2; 
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
 int main () {
   // run_recursion_case();
   // run_A_B_C_case();
@@ -132,6 +202,6 @@ int main () {
   // cout << (is_random_palindrom ? "random palindrom" : "random not palindrom") << endl;
   // cout << decimal_to_binary(2);
   // cout << sum_of_natural_numbers(10);
-  vector <int> arr = {1 , 2 , 3};
-  cout << recursive_binary_search(arr , 0 , 2 , 2) << endl;
+  // vector <int> arr = {1 , 2 , 3};
+  // cout << recursive_binary_search(arr , 0 , 2 , 2) << endl;
 }
