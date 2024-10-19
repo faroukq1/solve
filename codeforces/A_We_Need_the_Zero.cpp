1518f7b0bf11544cd141533bccbb629a7c34c958
faroukq1 <faroukq1@gmail.com>
2024-10-19 09:52:44 +0100

    A_We_Need_the_Zero

---
+++ codeforces/A_We_Need_the_Zero.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <numeric>
+#include <random>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <limits>
+#include <string>
+#include <limits.h>
+
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+
+void run_case () {
+    long long N , ans = 0;
+    cin >> N;
+    vector<long long> arr(N);
+    for (auto &a : arr) {
+        cin >> a;
+        ans ^= a;
+    }
+
+    if (N % 2 == 0)
+        cout << (ans == 0 ? 0 : -1) << endl;
+    else
+        cout << ans << endl;
+}
+int main() {
+    int tt;
+    cin >> tt;
+    while (tt--)
+        run_case();
+    return 0;
+}
