d4687a7451e88acf23159af40ab50c76a8418dea
faroukq1 <faroukq1@gmail.com>
2024-11-11 18:02:24 +0100

    add

---
+++ B_Olya_and_Game_with_Arrays.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+void run_case() {
+    int N;
+    cin >> N;
+    vector<int> arr(N);
+    for (auto &a : arr)
+        cin >> a;
+
+    sort(arr.begin(), arr.end() , [] (int a , int b) -> bool {return a > b});
+    for (auto &a : arr)
+        cout << a << ' ';
+    cout << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+#ifndef NEAL_DEBUG
+    cin.tie(nullptr);
+#endif
+
+    int tests;
+    cin >> tests;
+
+    while (tests-- > 0)
+        run_case();
+}
--- codeforces/A_Alice_s_Adventures_in_Chess.cpp
+++ codeforces/A_Alice_s_Adventures_in_Chess.cpp
@@ -25,32 +25,33 @@ template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 
 
-template<typename T_vector>
-void output_vector(const T_vector &v, bool add_one = false, int start = -1, int end = -1) {
-    if (start < 0) start = 0;
-    if (end < 0) end = int(v.size());
- 
-    for (int i = start; i < end; i++)
-        cout << v[i] + (add_one ? 1 : 0) << (i < end - 1 ? ' ' : '\n');
-}
-
-
-const int X_MAX = 505;
 
 void run_case() {
-    int N;
-    cin >> N;
-    vector<int> X(N , 0);
-    for (int i = 1 ; i < N ; i++)
-        cin >> X[i];
+    int n , a , b;
+    cin >> n >> a >> b;
+    string s;
+    cin >> s;
 
-    vector<int> A(N);
-    A[0] = X_MAX;
-
-    for (int i = 1 ; i < N ; i++)
-        A[i] = A[i - 1] + X[i];
-
-    output_vector(A);
+    int x = 0 , y = 0;
+    for (int i = 0 ; i < 40 ; i++) {
+        for (char c : s) {
+            if (c == 'N') {
+                y++;
+            } else if (c == 'E') {
+                x++;
+            } else if (c == 'S')  {
+                y--;
+            } else {
+                x--;
+            }
+            
+            if (x == a && y == b) {
+                cout << "YES" << endl;
+                return;
+            }
+        }
+    }
+    cout << "NO" << endl;
 }
 
 int main() {
