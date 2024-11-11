58041079e68d7aade1fa1ad7a3c53c29109069e9
faroukq1 <faroukq1@gmail.com>
2024-11-11 18:19:33 +0100

    add

--- codeforces/balancedround.cpp
+++ codeforces/balancedround.cpp
@@ -1,36 +1,64 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
 using namespace std;
 
-int main() {
-  int N;
-  cin >> N;
-  while (N--) {
-    int n, k;
-    cin >> n >> k;
-    vector<int> test(n);
-    for (auto &t : test)
-      cin >> t;
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 
-    if (test.size() < 2) {
-      cout << 0 << "\n";
-      continue;
-    }
+void run_case() {
+  int N , K;
+  cin >> N >> K;
+  vector<int> roundes(N);
+  for (auto &round : roundes) cin >> round;
 
-    sort(test.begin(), test.end());
-
-    int res = 1, cpt = 1;
-
-    for (int i = 1; i < n; i++) {
-      const int diff = test[i] - test[i - 1];
-      if (diff <= k)
-        cpt++;
-      else {
-        res = max(res, cpt);
-        cpt = 1;
-      }
-    }
-
-    res = max(res, cpt);
-    cout << (n - res) << "\n";
+  if (N < 2) {
+    cout << 0 << endl;
+    return;
   }
+
+  sort(roundes.begin(), roundes.end());
+  int cpt = 1 , most = 1;
+  for (int i = 0 ; i < N - 1; i++) {
+    if (abs(roundes[i] - roundes[i + 1]) <= K) {
+      cpt++;
+    } else {
+      most = max(most , cpt);
+      cpt = 1;
+    }
+  }
+
+  most = max(most , cpt);
+  cout << N - most << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+#ifndef NEAL_DEBUG
+    cin.tie(nullptr);
+#endif
+
+    int tests;
+    cin >> tests;
+
+    while (tests-- > 0)
+        run_case();
 }
