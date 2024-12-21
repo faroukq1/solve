e4aa927c5fc351204d96015bdc79bb5e80a34e7f
faroukq1 <faroukq1@gmail.com>
2024-12-21 21:32:17 +0100

    add

---
+++ B_Normal_Problem.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+void run_case() {
+    string s;
+    cin >> s;
+    const int N = s.length();
+    for (int i = 0 ; i < N ; i++)
+        if (s[i] == 'q')
+            s[i] = 'p';
+        else if (s[i] == 'p')
+            s[i] = 'q';
+
+    for (int i = N - 1; i >= 0 ; i--)
+        cout << s[i];
+
+    cout << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+#ifndef NEAL_DEBUG
+    cin.tie(nullptr);
+#endif
+
+    int tests;
+    cin >> tests;
+
+    while (tests-- > 0)
+        run_case();
+}
