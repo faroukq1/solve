d457d1e882d221638909afcdd6db460a1c8cc367
farouk ouledmeriem <126769881+faroukq1@users.noreply.github.com>
2025-02-13 22:34:18 +0100

    solve

---
+++ codeforces/A_Mocha_and_Math.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+void run_case() {
+    long long N;
+    cin >> N;
+    vector<long long> arr(N);
+    for (auto &a : arr)
+        cin >> a;
+
+    long long ans = arr.front();
+    for (int i = 1; i < N; i++)
+        ans = ans & arr[i];
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+#ifndef NEAL_DEBUG
+    cin.tie(nullptr);
+#endif
+
+    int tests;
+    cin >> tests;
+
+    while (tests-- > 0)
+        run_case();
+}
