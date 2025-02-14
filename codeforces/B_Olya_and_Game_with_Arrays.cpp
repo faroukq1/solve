6d9375d6820c893647af0700a0f6ddd0b06ced32
farouk ouledmeriem <126769881+faroukq1@users.noreply.github.com>
2025-02-14 16:45:07 +0100

    the slow solution

--- codeforces/B_Olya_and_Game_with_Arrays.cpp
+++ codeforces/B_Olya_and_Game_with_Arrays.cpp
@@ -8,9 +8,29 @@ template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
 }
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 
-int mn = INT_MAX;
 
-void run_case() {
+// void run_case() {
+//     int N;
+//     cin >> N;
+//     int mn = INT_MAX;
+//     vector<int> second;
+//     for (int i = 0; i < N; i++) {
+//         int K;
+//         cin >> K;
+//         vector<int> A(K);
+//         for (auto &a : A)
+//             cin >> a;
+
+//         sort(A.begin(), A.end());
+//         mn = min(A.front(), mn);
+//         second.push_back(A[1]);
+//     }
+
+//     long long ans = mn + accumulate(second.begin(), second.end(), 0LL) - *min_element(second.begin(), second.end());
+//     cout << ans << endl;
+// }
+
+long long solve () {
     int N;
     cin >> N;
     int mn = INT_MAX;
@@ -22,15 +42,14 @@ void run_case() {
         for (auto &a : A)
             cin >> a;
 
-        sort(A.begin(), A.end());
-        mn = min(A.front(), mn);
-        second.push_back(A[1]);
+        int min_A = *min_element(A.begin(), A.end());
+        mn = min(mn, min_A);
+        A.erase(find(A.begin(), A.end() , min_A));
+        second.push_back(*min_element(A.begin(), A.end()));
     }
 
-    long long ans = mn + accumulate(second.begin(), second.end(), 0LL) - *min_element(second.begin(), second.end());
-    cout << ans << endl;
+    return mn + accumulate(second.begin(), second.end(), 0) - *min_element(second.begin(), second.end());
 }
-
 int main() {
     ios::sync_with_stdio(false);
 #ifndef NEAL_DEBUG
@@ -40,6 +59,8 @@ int main() {
     int tests;
     cin >> tests;
 
-    while (tests-- > 0)
-        run_case();
+    while (tests-- > 0) {
+        // run_case();
+        cout << solve() << endl;
+    }
 }
