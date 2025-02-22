6dc202d9a932c64fd5e77df4330f7a3469d0c153
farouk ouledmeriem <126769881+faroukq1@users.noreply.github.com>
2025-02-22 18:43:05 +0100

    solve

--- kattis/A. Maximum Distance.cpp
+++ kattis/A. Maximum Distance.cpp
@@ -1,17 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dbg_out() { cerr << endl; }
-template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
-  cerr << ' ' << H;
-  dbg_out(T...);
-}
-#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-
-
 int64_t get_distance(int64_t x1, int64_t y1, int64_t x2, int64_t y2) {
-    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    int64_t dx = x2 - x1;
+    int64_t dy = y2 - y1;
+    return dx * dx + dy * dy;
 }
+
 void run_case() {
     int64_t N;
     cin >> N;
@@ -20,23 +15,16 @@ void run_case() {
     for (auto &x : X) cin >> x;
     for (auto &y : Y) cin >> y;
 
-    int64_t ans = INT64_MIN;
-    for (int i = 0; i < N; i++) {
-        // to do
-    }
+    int64_t ans = 0;
+    for (int i = 0; i < N; i++)
+        for (int j = i + 1; j < N; j++) ans = max(ans, get_distance(X[i], Y[i], X[j], Y[j]));
 
     cout << ans << endl;
 }
 
 int main() {
     ios::sync_with_stdio(false);
-#ifndef NEAL_DEBUG
     cin.tie(nullptr);
-#endif
 
-    int tests= 1;
-    // cin >> tests;
-
-    while (tests-- > 0)
-        run_case();
+    run_case();
 }
