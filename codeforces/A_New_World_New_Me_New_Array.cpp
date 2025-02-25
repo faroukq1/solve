1f4f21e3a4fdb37b6d73afa8167da2d0ea8973ba
farouk ouledmeriem <126769881+faroukq1@users.noreply.github.com>
2025-02-25 20:53:44 +0100

    div3 & solve

---
+++ codeforces/A_New_World_New_Me_New_Array.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+void run_case() {
+    // solving
+    int N, K, P;
+    cin >> N >> K >> P;
+
+    int div = abs(K) / abs(P);
+    int rem = abs(K) % abs(P);
+    // dbg(N, div, rem);
+    int ans = div + (rem != 0);
+    cout << (ans > N ? -1 : ans) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+#ifndef NEAL_DEBUG
+    cin.tie(nullptr);
+#endif
+
+    int tests;
+    cin >> tests;
+
+    while (tests-- > 0)
+        run_case();
+}
