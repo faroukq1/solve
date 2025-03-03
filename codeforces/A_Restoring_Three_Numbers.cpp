a149f0d08ba4d760b9cc1df680b4d258cff6df78
farouk ouledmeriem <126769881+faroukq1@users.noreply.github.com>
2025-03-03 21:40:59 +0100

    solve

--- codeforces/A_Restoring_Three_Numbers.cpp
+++ codeforces/A_Restoring_Three_Numbers.cpp
@@ -2,31 +2,32 @@
 using namespace std;
 
 void dbg_out() { cerr << endl; }
-template <typename Head, typename... Tail>
-void dbg_out(Head H, Tail... T)
-{
-    cerr << ' ' << H;
-    dbg_out(T...);
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
 }
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-using ll = long long;
-int main() {
-    vector<int> arr(4);
-    for (auto &a : arr)
-        cin >> a;
-    vector<int> sorted = arr;
-    sort(sorted.begin(),sorted.end());
-    int x = sorted[0] , y = sorted[1] , z = sorted[2] , h = sorted[3];
 
-    // x + c = h
-    // a + b = x
-    // a + c = y
-    // b + c = z
-    // -b + c = x + y
-    // c = ((x + y + z) / 2)
-    int c = ((x + y + z) / 2);
-    int a = y - c;
-    int b = z -c;
-    cout << a << ' ' << b << ' ' << c <<endl;
-    return 0;
+void run_case() {
+    // solving
+    vector<int> A(4);
+    for (auto &a : A)
+        cin >> a;
+
+    sort(A.begin(), A.end());
+
+    for (int i = 2; i >= 0; i--)
+        cout << A.back() - A[i] << (i == 0 ? '\n' : ' ');
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+#ifndef NEAL_DEBUG
+    cin.tie(nullptr);
+#endif
+
+    int tests = 1;
+
+    while (tests-- > 0)
+        run_case();
 }
