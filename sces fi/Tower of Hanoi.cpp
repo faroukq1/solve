c45f30260469a35a814092cfa97f076fb5f29ce9
faroukq1 <ouledmeriemfarouk1@gmail.com>
2025-08-02 08:08:53 +0100

    solve

--- sces fi/Tower of Hanoi.cpp
+++ sces fi/Tower of Hanoi.cpp
@@ -9,16 +9,22 @@ template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 
 
-void solve () {
-    /*
 
-    to do
+vector<pair<int, int>> moves;
+void solve (int N, int from, int to) {
+  if (N == 0) return;
 
-    */
+  int other = 6 - from - to;
+  solve(N - 1, from, other);
+  moves.emplace_back(from, to);
+  solve(N - 1, other, to);
 }
 
 int main () {
-    int test = 1;
-    // cin >> test;
-    while (test--)  solve();
+    int N;
+    cin >> N;
+    solve(N, 1, 3);
+    cout << moves.size() << endl;
+    for (auto &m : moves)
+      cout << m.first << ' ' << m.second << endl;
 }
