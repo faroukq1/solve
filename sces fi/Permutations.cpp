ee997befa16c24f2f92a4489344e59ef258569d3
faroukq1 <faroukq1@gmail.com>
2025-12-11 20:06:53 +0100

    back to solve again sces problem set

--- sces fi/Permutations.cpp
+++ sces fi/Permutations.cpp
@@ -1,39 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 void dbg_out() { cerr << endl; }
 template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
   cerr << ' ' << H;
   dbg_out(T...);
 }
-#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
- 
- 
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):" dbg_out(__VA_ARGS__)
+
+// Utility functions for comparison
+template <typename S, typename T> void smax(S &a, const T &b) {
+  if (a < b)
+    a = b;
+}
+template <typename S, typename T> void smin(S &a, const T &b) {
+  if (a > b)
+    a = b;
+}
+
+// Random number generator setup
+#define rng_init                                                               \
+  mt19937 rng(chrono::steady_clock::now().time_since_epoch().count())
+#define rng_seed(x) mt19937 rng(x)
+#define rng(x) rng(x)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+const int MXN = 1e5 + 5, INF = 1e9 + 5;
+
+
+
 void solve () {
     int64_t N;
     cin >> N;
-    if (N == 1)
-        return void(cout << 1 << endl);
- 
+    if (N > 1 && N < 4)
+        return void(cout << "NO SOLUTION" << endl);
+
     if (N == 4)
         return void(cout << "2 4 1 3" << endl);
- 
-    
-    if (N <= 3)
-        return void(cout << "NO SOLUTION" << endl);
- 
-    for (int64_t i = 1; i <= N; i+=2)
+
+    for (int i = 1; i <= N; i+=2)
         cout << i << ' ';
-    for (int64_t i = 2; i <= N; i+=2)
+    for (int i = 2; i <= N; i+=2)
         cout << i << ' ';
- 
+
     cout << endl;
- 
-    
 }
- 
-int main () {
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
     int test = 1;
-    // cin >> test;
-    while (test--)  solve();
+    while (test--)
+        solve();
 }
