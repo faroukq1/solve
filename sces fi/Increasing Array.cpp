572e9847d1fa6b3eb1e8d0b80c10ec325f002d3d
faroukq1 <faroukq1@gmail.com>
2025-12-11 16:30:31 +0100

    back to solving again scres problem set

--- sces fi/Increasing Array.cpp
+++ sces fi/Increasing Array.cpp
@@ -6,31 +6,51 @@ template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
   cerr << ' ' << H;
   dbg_out(T...);
 }
-#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):" dbg_out(__VA_ARGS__)
+
+// Utility functions for comparison
+template <typename S, typename T> void smax(S &a, const T &b) {
+  if (a < b)
+    a = b;
+}
+template <typename S, typename T> void smin(S &a, const T &b) {
+  if (a > b)
+    a = b;
+}
+
+// Random number generator setup
+#define rng_init                                                               \
+  mt19937 rng(chrono::steady_clock::now().time_since_epoch().count())
+#define rng_seed(x) mt19937 rng(x)
+#define rng(x) rng(x)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+const int MXN = 1e5 + 5, INF = 1e9 + 5;
+
 
 
 void solve () {
     int N;
     cin >> N;
-    vector<int64_t> A(N);
-    for (auto &a : A)
-        cin >> a;
+    vector<int64_t> store(N);
+    for (auto &s : store)
+        cin >> s;
 
     int64_t ans = 0;
-    for (int i = 0; i < N; i++) {
-        if (i != 0 && A[i] < A[i - 1]) {
-            const int64_t diff = abs(A[i] - A[i - 1]);
-
-            ans += diff;
-            A[i] += diff;
+    for (int i = 0; i < N - 1; i++)
+        if (store[i] > store[i + 1]) {
+            ans += store[i] - store[i + 1];
+            store[i + 1] = store[i];
         }
-    }
 
     cout << ans << endl;
 }
 
-int main () {
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
     int test = 1;
     // cin >> test;
-    while (test--)  solve();
+    while (test--)
+        solve();
 }
