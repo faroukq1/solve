6404f5233fb3e124d93db98ff9580bc5a5d5b7a8
faroukq1 <faroukq1@gmail.com>
2025-12-11 16:16:48 +0100

    back to solve sces problem set

--- sces fi/Missing Number.cpp
+++ sces fi/Missing Number.cpp
@@ -6,28 +6,50 @@ template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
   cerr << ' ' << H;
   dbg_out(T...);
 }
-#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):" dbg_out(__VA_ARGS__)
+
+// Utility functions for comparison
+template <typename S, typename T> void smax(S &a, const T &b) {
+  if (a < b)
+    a = b;
+}
+template <typename S, typename T> void smin(S &a, const T &b) {
+  if (a > b)
+    a = b;
+}
+
+// Random number generator setup
+#define rng_init                                                               \
+  mt19937 rng(chrono::steady_clock::now().time_since_epoch().count())
+#define rng_seed(x) mt19937 rng(x)
+#define rng(x) rng(x)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+const int MXN = 1e5 + 5, INF = 1e9 + 5;
+
 
 
 void solve () {
     int64_t N;
     cin >> N;
+    vector<bool> exist(N + 1, false);
 
-    const int64_t full_sum = (N * (N + 1)) / 2;
-
-    int64_t partial_sum = 0;
     for (int i = 0; i < N - 1; i++) {
-        int64_t a;
+        int a;
         cin >> a;
-        // dbg(a);
-        partial_sum += a;
+        exist[a] = true;
     }
 
-    cout << full_sum - partial_sum << endl;
+    for (int i = 1; i < N + 1; i++)
+        if (!exist[i])
+            cout << i << ' ';
 }
 
-int main () {
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
     int test = 1;
     // cin >> test;
-    while (test--)  solve();
+    while (test--)
+        solve();
 }
