473cd7effe466bf934512a8c94593ff1b8c7d794
faroukq1 <faroukq1@gmail.com>
2025-12-11 16:20:12 +0100

    back to solve again sces problem set

--- sces fi/Repetitions.cpp
+++ sces fi/Repetitions.cpp
@@ -6,29 +6,54 @@ template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
   cerr << ' ' << H;
   dbg_out(T...);
 }
-#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):" dbg_out(__VA_ARGS__)
+
+// Utility functions for comparison
+template <typename S, typename T> void smax(S &a, const T &b) {
+  if (a < b)
+    a = b;
+}
+template <typename S, typename T> void smin(S &a, const T &b) {
+  if (a > b)
+    a = b;
+}
+
+// Random number generator setup
+#define rng_init                                                               \
+  mt19937 rng(chrono::steady_clock::now().time_since_epoch().count())
+#define rng_seed(x) mt19937 rng(x)
+#define rng(x) rng(x)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+const int MXN = 1e5 + 5, INF = 1e9 + 5;
+
 
 
 void solve () {
     string s;
     cin >> s;
-    const long long N = s.length();
-    long long current_sum = 1;
-    long long ans = 1;
-    for (int i = 0; i < N; i++) {
-        if (i != 0 && s[i] == s[i - 1])
-            current_sum++;
+    const int N = int(s.size());
+
+    int current = 1;
+    int ans = 0;
+    for (int i = 0; i < N - 1; i++) {
+        if (s[i] == s[i + 1])
+            current++;
         else {
-            ans = max(ans, current_sum);
-            current_sum = 1;
+            ans = max(current, ans);
+            current = 1;
         }
     }
-    cout << max(ans, current_sum) << endl;
 
+    
+    cout << max(ans, current) << endl;
 }
 
-int main () {
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
     int test = 1;
     // cin >> test;
-    while (test--)  solve();
+    while (test--)
+        solve();
 }
